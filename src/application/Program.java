6d834e629cd4eb2234666a603599ce1df2fb092d
Alison Vieira <alisonvini@gmail.com>
2023-05-23 07:02:08 -0300

    Problem solution(without interface) - PART 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,28 +7,44 @@ import java.util.Scanner;
 
 import entities.CarRental;
 import entities.Vehicle;
+import model.services.BrazilTaxService;
+import model.services.RentalService;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		
+
 		System.out.println("Entre com os dados do aluguel");
 		System.out.println("Modelo do carro");
-		String carModel = sc.nextLine();		
-		System.out.println("Retirada(dd/mm/yyyy hh:mm)");
+		String carModel = sc.nextLine();
+		System.out.print("Retirada(dd/mm/yyyy hh:mm)");
 		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.println("Retorno(dd/mm/yyyy hh:mm)");
+		System.out.print("Retorno(dd/mm/yyyy hh:mm)");
 		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
 		
-	   CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+		System.out.println("Entre com o preço por hora");
+		double pricePerHour = sc.nextDouble();
+		System.out.println("Entre com o preço por dia");
+		double pricePerDay = sc.nextDouble();
+		
+		RentalService rentaService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+		
+		rentaService.processInvoice(cr);
+		
+		System.out.println("FATURA:");
+		System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+		System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+		System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
 		
 		sc.close();
-		
+
 	}
 
 }
